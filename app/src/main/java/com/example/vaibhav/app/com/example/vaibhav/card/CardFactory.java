ede3ffd2537aa863721d16a9800ef21a6d463f66
Feroz <feroz@istarindia.com>
2016-11-23 12:30:58 +0530

    adding cms template

--- app/src/main/java/com/example/vaibhav/app/com/example/vaibhav/card/CardFactory.java
+++ app/src/main/java/com/example/vaibhav/app/com/example/vaibhav/card/CardFactory.java
@@ -8,22 +8,27 @@ import com.example.vaibhav.app.com.example.vaibhav.card.adapter.DummyFragment;
 
 public class CardFactory {
     public static Card  getCard(String cradType) {
-        if(cradType.equalsIgnoreCase("ONLY_TITLE")){
-            return new ONLYTITLE();
-        }else if(cradType.equalsIgnoreCase("ONLY_TITLE_IMAGE")){
-            return new ONLYTITLEIMAGE();
-        }
-        else if(cradType.equalsIgnoreCase("ONLY_TITLE_LIST")){
-            return new ONLYTITLELISTRECYCYLE();
-        }
-        else if(cradType.equalsIgnoreCase("ONLY_TITLE_IMAGE")){
-            return new ONLY_TITLE_IMAGE();
-        }
-        else if(cradType.equalsIgnoreCase("ONLY_TITLE_PARAGRAPH")){
-            return new ONLY_TITLE_PARAGRAPH();
-        }
-        //ONLY_TITLE_PARAGRAPH
-        else{
+        if(cradType != null) {
+
+            if (cradType.equalsIgnoreCase("ONLY_TITLE")) {
+                return new ONLYTITLE();
+            } else if (cradType.equalsIgnoreCase("ONLY_TITLE_IMAGE")) {
+                return new ONLYTITLEIMAGE();
+            } else if (cradType.equalsIgnoreCase("ONLY_TITLE_LIST")) {
+                return new ONLYTITLELISTRECYCYLE();
+            } else if (cradType.equalsIgnoreCase("ONLY_TITLE_IMAGE")) {
+                return new ONLY_TITLE_IMAGE();
+            } else if (cradType.equalsIgnoreCase("ONLY_TITLE_PARAGRAPH")) {
+                return new ONLY_TITLE_PARAGRAPH();
+            } else if (cradType.equalsIgnoreCase("ONLY_TITLE_PARAGRAPH_IMAGE")) {
+                return new ONLY_TITLE_PARAGRAPH_IMAGE();
+            }
+            //ONLY_TITLE_PARAGRAPH_IMAGE
+            else {
+                return new DummyFragment();
+
+            }
+        }else {
             return new DummyFragment();
 
         }
--- app/src/main/java/com/example/vaibhav/app/com/example/vaibhav/card/ONLY_TITLE_PARAGRAPH_IMAGE.java
+++ app/src/main/java/com/example/vaibhav/app/com/example/vaibhav/card/ONLY_TITLE_PARAGRAPH_IMAGE.java
@@ -5,21 +5,53 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.vaibhav.app.R;
+import com.example.vaibhav.app.cmspojo.CMSSlide;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Feroz on 23/11/2016.
  */
 
 public class ONLY_TITLE_PARAGRAPH_IMAGE extends Card {
+    private TextView title,paragraph;
+    private ImageView image;
+    private Picasso mPicasso;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.only_title_paragraph_image, container, false);
+        title = (TextView) view.findViewById(R.id.title);
+        paragraph = (TextView) view.findViewById(R.id.paragraph);
+        image = (ImageView) view.findViewById(R.id.image);
+        mPicasso = Picasso.with(getContext()); //Single instance
 
+        if (getArguments() != null) {
 
-        return view;
+            CMSSlide cms = null;
+            if(getArguments().getSerializable("CMSSLIDE") != null)
+                cms= (CMSSlide) getArguments().getSerializable("CMSSLIDE");
+
+            if(cms != null){
+                if(cms.getTitle() != null && cms.getTitle().getText() != null)
+                    title.setText(cms.getTitle().getText());
+
+                if(cms.getParagraph() != null && cms.getParagraph().getText() != null){
+                    paragraph.setText(cms.getParagraph().getText());
+                }
+
+                if(cms.getImage() != null && cms.getImage().getUrl() != null){
+                    mPicasso.load("http://api.talentify.in" + cms.getImage().getUrl()).into(image);
+
+                }
+
+            }
+        }
+
+            return view;
     }
 }
