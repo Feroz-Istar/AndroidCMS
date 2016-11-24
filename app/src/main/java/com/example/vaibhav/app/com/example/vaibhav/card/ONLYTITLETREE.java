7654c3c6eb222636ec278b90a4e18b048b4c263b
Sumanth <sumanth@istarindia.com>
2016-11-24 21:17:22 +0530

    Templets

--- app/src/main/java/com/example/vaibhav/app/com/example/vaibhav/card/ONLYTITLETREE.java
+++ app/src/main/java/com/example/vaibhav/app/com/example/vaibhav/card/ONLYTITLETREE.java
@@ -2,6 +2,9 @@ package com.example.vaibhav.app.com.example.vaibhav.card;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,40 +12,54 @@ import android.widget.TextView;
 
 import com.example.vaibhav.app.R;
 import com.example.vaibhav.app.cmspojo.CMSSlide;
+import com.example.vaibhav.app.com.example.vaibhav.card.adapter.OnlyTitleTreeRecycleAdapter;
 import com.example.vaibhav.app.mediautility.ImageSaver;
 import com.example.vaibhav.app.util.CustomLayout;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Sumanth on 11/23/2016.
  */
 public class ONLYTITLETREE extends Card {
 
-    private TextView paragraph,title;
+    private RecyclerView ParentrecyclerView;
+    private TextView textView;
+    private OnlyTitleTreeRecycleAdapter onlytitletreeRecycleAdapter;
     private Picasso mPicasso;
     private CustomLayout main_layout;
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.only_title_tree, container, false);
-        paragraph = (TextView) view.findViewById(R.id.paragraph);
-        title = (TextView) view.findViewById(R.id.title);
-        mPicasso = Picasso.with(getContext());
-        main_layout = (CustomLayout) view.findViewById(R.id.main_layout);
+        textView = (TextView) view.findViewById(R.id.text);
+        mPicasso = Picasso.with(getContext()); //Single instance
         Boolean externalReadable = ImageSaver.isExternalStorageReadable();
+        main_layout = (CustomLayout) view.findViewById(R.id.main_layout);
+
+        ParentrecyclerView = (RecyclerView) view.findViewById(R.id.only_title_tree_parent_itemListRV);
+
         if (getArguments() != null) {
-            CMSSlide cms = null;
-            cms= (CMSSlide) getArguments().getSerializable("CMSSLIDE");
-            if(cms != null) {
+            CMSSlide cms = (CMSSlide)getArguments().getSerializable("CMSSLIDE");
+            if(cms.getList() != null){
+
                 ThemeUtils themeUtils = new ThemeUtils();
-                themeUtils.massageTitle(cms, title, getContext());
-                themeUtils.massageTreeList(cms, paragraph, getContext());
-                themeUtils.massageBackgroundLayout(cms, mPicasso, main_layout, externalReadable, getContext());
+                themeUtils.massageTitle(cms,textView,getContext());
+                themeUtils.massageBackgroundLayout(cms,mPicasso,main_layout,externalReadable,getContext());
+
+                onlytitletreeRecycleAdapter = new OnlyTitleTreeRecycleAdapter(cms,getContext());
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+                ParentrecyclerView.setLayoutManager(mLayoutManager);
+                ParentrecyclerView.setItemAnimator(new DefaultItemAnimator());
+                ParentrecyclerView.setAdapter(onlytitletreeRecycleAdapter);
+
             }
 
         }
 
-        return view;
+        return view ;
+
     }
-    }
+}
---
+++ app/src/main/java/com/example/vaibhav/app/com/example/vaibhav/card/adapter/OnlyTitleTreeRecycleAdapter.java
@@ -0,0 +1,86 @@
+package com.example.vaibhav.app.com.example.vaibhav.card.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.vaibhav.app.R;
+import com.example.vaibhav.app.cmspojo.CMSSlide;
+import com.example.vaibhav.app.cmspojo.CMSTextItem;
+import com.example.vaibhav.app.util.BulletListBuilder;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Feroz on 21/11/2016.
+ */
+
+public class OnlyTitleTreeRecycleAdapter extends RecyclerView.Adapter<OnlyTitleTreeRecycleAdapter.MyViewHolder> {
+
+    private CMSSlide cmsSlide;
+    private  Context context;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title1,title2;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title1 = (TextView) view.findViewById(R.id.title1);
+            title2 = (TextView) view.findViewById(R.id.title2);
+        }
+    }
+
+
+    public OnlyTitleTreeRecycleAdapter(CMSSlide cmsSlide , Context context) {
+
+        this.cmsSlide = cmsSlide;
+        this.context = context;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycle_tree_list_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        String listitems = cmsSlide.getList().getItems().get(position).getText();
+        ArrayList<String> bulletList = new ArrayList<>();
+
+        for(CMSTextItem list : cmsSlide.getList().getItems().get(position).getList().getItems()){
+            if(list != null && list.getText() != null)
+                bulletList.add(list.getText());
+
+        }
+
+        Typeface paragraphtf = Typeface.createFromAsset(context.getAssets(), "Raleway-Regular.ttf");
+
+        holder.title1.setTypeface(paragraphtf);
+        holder.title1.setTextColor(Color.parseColor(cmsSlide.getTheme().getParagraphFontColor()));
+        holder.title1.setTextSize(Integer.parseInt(cmsSlide.getTheme().getParagraphFontSize()) / 3);
+        holder.title1.setText(listitems);
+
+        if(bulletList.size() > 0)
+
+        holder.title2.setTypeface(paragraphtf);
+        holder.title2.setTextColor(Color.parseColor(cmsSlide.getTheme().getParagraphFontColor()));
+        holder.title2.setTextSize(Integer.parseInt(cmsSlide.getTheme().getParagraphFontSize()) / 3);
+
+        holder.title2.setText(new BulletListBuilder(context).getBulletList(bulletList,"",15));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cmsSlide.getList().getItems().size();
+    }
+}
